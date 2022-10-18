e36d653c469b3faef448eb349079147b2c2aa3aa
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-18 10:13:16 +0100

    initialized project set up with necessary files

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBSXI3SVndynFbyUFrA_i7TlnvZsl7t_b0",
+    authDomain: "cloud-news-4f403.firebaseapp.com",
+    projectId: "cloud-news-4f403",
+    storageBucket: "cloud-news-4f403.appspot.com",
+    messagingSenderId: "756802611530",
+    appId: "1:756802611530:web:df2ffbf11fd4b3f216b9b3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
