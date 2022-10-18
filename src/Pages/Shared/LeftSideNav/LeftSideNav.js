57a21f9a71921d52d1fc3b3d690fb90c0c6e727a
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-18 13:22:41 +0100

    loaded data from cloud-news-server and diaplayed in LeftSideNav bar as dynamic link

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+            .catch(error => console.error(error));
+    }, [])
+
+
     return (
         <div>
-            <h2>left side nav</h2>
+            <h5>All Categories: {categories.length}</h5>
+            <div>
+                {
+                    categories.map(category =>
+                        <p key={category.id}>
+                            <Link to={`/category/${category.id}`}>
+                                {category.name}
+                            </Link>
+                        </p>)
+                }
+            </div>
         </div>
     );
 };
