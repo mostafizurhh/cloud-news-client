bed0ea5f099306020579eca082bc6c07b920b6c6
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-18 11:00:19 +0100

    created basic layout of the webpage

---
+++ src/_steps.js
@@ -0,0 +1,13 @@
+/* 
+------------------------
+BASIC PROJECT SETUP
+------------------------
+1. create firebase app
+2. get firebase configuration inside firebase.config.js and export app from it
+3. install firebase, react-router-dom, react-bootstrap and react
+4. import bootstrap css file inside index.js
+
+---------------------
+ROUTER SETUP
+---------------------
+*/
