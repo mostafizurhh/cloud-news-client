6e92333a18cef3503fc8a4cc02eaf785a0084e96
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-20 00:37:40 +0100

    displayed news details under News component

--- src/Pages/News/News/News.js
+++ src/Pages/News/News/News.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { FaStar } from 'react-icons/fa';
 
 const News = () => {
+    const news = useLoaderData();
+    // console.log(news)
+    const { category_id, title, details, image_url, author } = news
     return (
-        <div>
-            <h2>This is news</h2>
-        </div>
+        <Card>
+            <Card.Img variant="top" src={image_url} />
+            <Card.Body>
+                <div className=' mb-3'>
+                    <Card.Title className='fw-normal'>{title}</Card.Title>
+                    <div className='d-flex justify-content-between'>
+                        <small><b>Author:</b> {author?.name}</small>
+                        <small><b>Published Date:</b> {author?.published_date}</small>
+                        <div className='d-flex align-items-center'>
+                            <FaStar style={{ color: 'orange', marginRight: 5 }}></FaStar>
+                            <small>{news.rating.number}</small>
+                        </div>
+                    </div>
+
+                </div>
+                <Card.Text>
+                    {details}
+                </Card.Text>
+                <Link to={`/category/${category_id}`}>
+                    <Button variant="primary">All News in This Category</Button>
+                </Link>
+            </Card.Body>
+        </Card>
     );
 };
 
--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -1,38 +1,46 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import './NewsSummaryCard.css'
-import { FaBookmark, FaEye, FaShareAlt, FaStar } from "react-icons/fa";
+import { FaRegBookmark, FaEye, FaShareAlt, FaStar } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const NewsSummaryCard = ({ news }) => {
-    console.log(news)
+    // console.log(news)
     return (
         <Card className="text-center mb-5">
             <Card.Header className='d-flex align-items-center justify-content-between'>
                 <div className='d-flex align-items-center'>
                     <div className='author'>
-                        <img src={news.author.img} alt="" />
+                        <img src={news.author?.img} alt="" />
                     </div>
                     <div className='d-flex flex-column align-items-start justify-content-start'>
-                        <small><small>{news.author.name}</small></small>
-                        <small><small>{news.author.published_date}</small></small>
+                        <small><small>{news.author?.name}</small></small>
+                        <small><small>{news.author?.published_date}</small></small>
                     </div>
                 </div>
                 <div>
-                    <FaBookmark className='me-1'></FaBookmark>
+                    <FaRegBookmark className='me-1'></FaRegBookmark>
                     <FaShareAlt></FaShareAlt>
                 </div>
             </Card.Header>
             <Card.Body>
                 <Card.Title className='mb-3'>{news.title}</Card.Title>
-                <Card.Img src={news.thumbnail_url} className='border rounded-5' />
+                <Card.Img src={news.image_url} />
+
+                {/* card details conditional formating */}
                 <Card.Text className='mt-3 text-start'>
-                    {news.details}
+                    {
+                        news.details.length > 180 ?
+                            <p>{news.details.slice(0, 180) + '...'} <Link to={`/news/${news._id}`}>Read more</Link></p>
+                            :
+                            <p>{news.details}</p>
+                    }
                 </Card.Text>
             </Card.Body>
             <Card.Footer className="d-flex align-items-center justify-content-between">
                 <div className='d-flex align-items-center'>
                     <FaStar style={{ color: 'orange', marginRight: 5 }}></FaStar>
-                    <small>4.7</small>
+                    <small>{news.rating.number}</small>
                 </div>
                 <div className='d-flex align-items-center'>
                     <FaEye style={{ marginRight: 5 }}></FaEye>
