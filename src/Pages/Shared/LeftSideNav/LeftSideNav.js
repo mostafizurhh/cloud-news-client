32b130b1eea44aca7c78ae7e18845b97a4bd8fdc
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-21 18:03:55 +0100

    loaded data from vercel instead of local server

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -5,7 +5,7 @@ const LeftSideNav = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/categories')
+        fetch('https://cloud-news-server.vercel.app/categories')
             .then(res => res.json())
             .then(data => setCategories(data))
             .catch(error => console.error(error));
